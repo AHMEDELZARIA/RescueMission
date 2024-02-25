1348acc2e26aab67f9edeb4669ce8c72123d6632
neha-bhatla <nehab2708@gmail.com>
2024-02-25 16:22:29 -0500

    Implements methods within Radar and Report (close #7)

--- src/main/java/ca/mcmaster/se2aa4/island/team220/ExpenseTracker.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/ExpenseTracker.java
@@ -1,13 +1,16 @@
 package ca.mcmaster.se2aa4.island.team220;
 
 public class ExpenseTracker{
+    //initialize public variable totalExpense as type int
     public int totalExpense;
 
     public void expenseTracker(){
+        //initialize totalExpense to 0
         this.totalExpense = 0;
     }
 
     public int getCost(int cost){
+        //adds the cost to the totalExpense and then returns totalExpense
         totalExpense += cost;
         return totalExpense;
     }
--- src/main/java/ca/mcmaster/se2aa4/island/team220/Report.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/Report.java
@@ -2,4 +2,8 @@ package ca.mcmaster.se2aa4.island.team220;
 
 public class Report{
 
+    public String deliverReport(String report){
+        //method returns the final report
+        return report;
+    }
 }
