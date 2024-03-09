1cfbd5a25798b0ccd22321a2e91f02d5df1fe2ca
Ahmed Elzaria <aelzaria@gmail.com>
2024-03-09 11:11:59 -0500

    Structural uml diagram created, close #9

--- src/main/java/ca/mcmaster/se2aa4/island/team220/Report.java
+++
@@ -1,8 +0,0 @@
-package ca.mcmaster.se2aa4.island.team220;
-
-public class Report{
-    public String deliverReport(String report){
-        //method returns the final report
-        return report;
-    }
-}
--- src/main/java/ca/mcmaster/se2aa4/island/team220/SourceInstructions.java
+++
@@ -1,7 +0,0 @@
-package ca.mcmaster.se2aa4.island.team220;
-
-public interface SourceInstructions {
-    String nextAction();
-
-    String prevAction();  
-}
