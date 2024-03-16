a2496f3fd993da14c0135762b1e8c4013de849c6
Anindita Bornomala <aninborno@LAPTOP>
2024-03-16 05:11:42 -0400

    Added FindIsland Algorithm to Gridsearch class (close #17)

--- src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
@@ -9,7 +9,24 @@ public class GridSearch implements ISearchAlgorithm {
     }
 
 
-    /* GridSearch Algorithm:
+    /*Gridsearch Algorithm: Assume we're facing Island
+     * 1. While SITE not found:
+     * 2. loop scan & fly, until SCAN = OCEAN
+     * 3. echo left & fly, until echo left = OCEAN
+     * 4. heading left x2
+     * 5. echo straight
+     * 6. if echo straight = LAND -> repeat steps 2-5
+     * 7. elif echo straight = OCEAN -> heading right x3
+     * from here, the next steps are similar to steps 2-4, except instead of 'left', we use 'right'
+     * 8. loop scan & fly, until SCAN = OCEAN
+     * 9. echo right & fly, until echo right = OCEAN
+     * 10. heading right x2
+     * 
+     * If at any point scan = SITE, stop search and return home
+    */
+
+
+    /* FindIsland Algorithm:
      * While Island not found:
      * echo straight (depends on heading)
      * if echo straight = island found -> fly there (# of fly commands = # of range tiles)
