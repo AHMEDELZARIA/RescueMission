a7bf94fba5ece4ea6c0ef7ef6e4fcb1cfe80c832
Anindita Bornomala <aninborno@LAPTOP>
2024-03-16 05:01:43 -0400

    Outlined GridSearch Algorithm (close #16)

--- src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
@@ -8,4 +8,19 @@ public class GridSearch implements ISearchAlgorithm {
         throw new UnsupportedOperationException("Unimplemented method 'determineDecision'");
     }
 
+
+    /* GridSearch Algorithm:
+     * While Island not found:
+     * echo straight (depends on heading)
+     * if echo straight = island found -> fly there (# of fly commands = # of range tiles)
+     * else, continue
+     * echo right (depends on heading)
+     * if echo right = island found -> heading right (depends on heading), fly there
+     * else, continue
+     * echo left (depends on heading)
+     * if echo left = island found -> heading left (depends on heading), fly there
+     * else, continue
+     * fly straight (if island not found after one round of echoing in all directions)
+    */
+
 }
