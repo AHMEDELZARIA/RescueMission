d922d9fa2df635a57198d6cffc252c0863151c47
neha-bhatla <nehab2708@gmail.com>
2024-03-17 13:14:07 -0400

    gridsearch logic from main

--- src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
@@ -7,5 +7,21 @@ public class GridSearch implements IDecisionHandler {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'determineDecision'");
     }
+
+        /*Gridsearch Algorithm: Assume we're facing Island
+     * 1. While SITE not found:
+     * 2. loop scan & fly, until SCAN = OCEAN
+     * 3. echo left & fly, until echo left = OCEAN
+     * 4. heading left x2
+     * 5. echo straight
+     * 6. if echo straight = LAND -> repeat steps 2-5
+     * 7. elif echo straight = OCEAN -> heading right x3
+     * from here, the next steps are similar to steps 2-4, except instead of 'left', we use 'right'
+     * 8. loop scan & fly, until SCAN = OCEAN
+     * 9. echo right & fly, until echo right = OCEAN
+     * 10. heading right x2
+     * 
+     * If at any point scan = SITE, stop search and return home
+    */
     
 }
