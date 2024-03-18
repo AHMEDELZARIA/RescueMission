dec154ef33265b629825e38d37930c164473632e
neha-bhatla <nehab2708@gmail.com>
2024-03-18 17:26:07 -0400

    comment that plans refactoring

--- src/main/java/ca/mcmaster/se2aa4/island/team220/Action.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/Action.java
@@ -1,6 +1,7 @@
 package ca.mcmaster.se2aa4.island.team220;
 
 public enum Action {
+    //refactor gridSearch by hiding instances of decision.put in a map in class Action.java
     FLY,
     ECHO,
     SCAN,
