f2ea796af1f0f16ea3f635cd76df3222af796d16
neha-bhatla <nehab2708@gmail.com>
2024-03-18 16:37:14 -0400

    added DecisionHandler from main

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/DecisionHandler.java
@@ -0,0 +1,9 @@
+package ca.mcmaster.se2aa4.island.team220;
+
+public class DecisionHandler {
+
+    private State currentState;
+
+    public void determineDecision(AreaMap map, Drone drone) {
+    };
+}
