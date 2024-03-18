b741e5c5fc569270d3bb8a3b0ca72477b8cc8921
neha-bhatla <nehab2708@gmail.com>
2024-03-18 16:09:46 -0400

    added comments to each method in gridSearch to explain the steps

--- src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
@@ -29,31 +29,36 @@ public class GridSearch implements IDecisionHandler {
 
     public boolean reachIsland(){
         return true;
+        //only repeated if in interlaceB interlaceC1 interlaceC2
     }
 
     public int searchSite(){
         return 0;
+        //always happens after reachIsland is called
     }
 
     public void intoPosition(){
-
+        //always follows searchSite
     }
 
     public void interlaceA(){
-
+        //always follows intoPosition
     }
 
     public void interlaceB(){
-
+        //always happens after interlaceA, and after excecuted, depending on what echo observes we either go interlaceC1, interlaceC2, go back to reachIsland or we stop
+        //if condtion is met at interlaceB we stop
     }
 
     public boolean interlaceC1(){
         return true;
+        //occurs if the number of searchSitesCount is odd
     }
 
     //either call interlaceC1 or interlaceC2
     public boolean interlaceC2(){
         return true;
+        //occurs if the number of searchSitesCount is even
     }
 
     public void executeGridSearch(){
