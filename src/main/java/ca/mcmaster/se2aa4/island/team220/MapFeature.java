83026d617ca913dc977949f4849c14f8fc13bc9d
neha-bhatla <nehab2708@gmail.com>
2024-03-18 18:49:39 -0400

    adds map feature class

--- src/main/java/ca/mcmaster/se2aa4/island/team220/MapFeature.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/MapFeature.java
@@ -1,8 +1,10 @@
 package ca.mcmaster.se2aa4.island.team220;
 
+// Need some sort of mechanism that groups all different land biomes under the enum constant LAND for example
 public enum MapFeature {
     OCEAN,
     CREEK,
     EMERGENCYSITE,
-    LAND;
+    LAND,
+    UNKNOWN; // Used when we discover a new coordinate, but haven't scanned to know what biome it is
 }
