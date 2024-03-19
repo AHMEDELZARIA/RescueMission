ebcb64a391552b7f5dbb980244475309161abab6
Anindita-Bornomala <aninborno@LAPTOP>
2024-03-19 13:18:23 -0400

    Testing config username

--- src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
@@ -53,5 +53,6 @@ public class CommandBook {
         }
 
         this.command.put(action, decision); // putting decision into the HashMap, an ex. of action (the key) is Action.STOP
+        logger.info("yippee");
     }
 }
