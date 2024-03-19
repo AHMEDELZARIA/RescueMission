3c645913571030ce019c4cf466c9390ca8d21c3a
Anindita-Bornomala <anindita.bornomala@gmail.com>
2024-03-19 13:54:50 -0400

    Updated CommandBook methods to work properly

--- src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
@@ -6,8 +6,10 @@ import org.json.JSONObject;
 public class CommandBook {
 
     private HashMap<Action, JSONObject> command;
+    private Compass compass;
     
-    public CommandBook(Compass compass) {
+    public CommandBook() {
+        this.compass = new Compass();
         this.command = new HashMap<>();
         buildAction(Action.STOP, null);
         buildAction(Action.FLY, null);
@@ -20,19 +22,84 @@ public class CommandBook {
         buildAction(Action.HEADING_RIGHT, compass.turnRight().toString());
     }
 
-    public JSONObject getStop() { return command.get(Action.STOP); }
+    public String getStop() {
+        JSONObject decision = new JSONObject();
+        decision.put("action", "stop");
+        return decision.toString();
+    }
 
-    public JSONObject getFly() { return command.get(Action.FLY); }
+    public String getFly() {
+        JSONObject decision = new JSONObject();
+        decision.put("action", "fly");
+        return decision.toString();
+    }
     
-    public JSONObject getScan() { return command.get(Action.SCAN); }
+    public String getScan() {
+        JSONObject decision = new JSONObject();
+        decision.put("action", "scan");
+        return decision.toString();
+    }
 
-    public JSONObject getEchoNorth() { return command.get(Action.ECHO_NORTH); }
+    public String getEchoNorth() {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
 
-    public JSONObject getEchoEast() { return command.get(Action.ECHO_EAST); }
+        decision.put("action", "echo");
+        decision.put("parameters", parameters.put("direction", 'N'));
 
-    public JSONObject getEchoSouth() { return command.get(Action.ECHO_SOUTH); }
+        return decision.toString();
+    }
 
-    public JSONObject getEchoWest() { return command.get(Action.ECHO_WEST); }
+    public String getEchoEast() {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
+
+        decision.put("action", "echo");
+        decision.put("parameters", parameters.put("direction", 'E'));
+
+        return decision.toString();
+    }
+
+    public String getEchoSouth() {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
+
+        decision.put("action", "echo");
+        decision.put("parameters", parameters.put("direction", 'S'));
+
+        return decision.toString();
+    }
+
+    public String EchoWest() {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
+
+        decision.put("action", "echo");
+        decision.put("parameters", parameters.put("direction", 'W'));
+
+        return decision.toString();
+    }
+
+    public String getTurnLeft(Compass compass) {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
+
+        decision.put("action", "fly");
+        decision.put("parameters", parameters.put("direction", this.compass.turnLeft()));
+
+        return decision.toString();
+    }
+
+    public String getTurnRight(Compass compass) {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
+
+        decision.put("action", "fly");
+        decision.put("parameters", parameters.put("direction", this.compass.turnRight()));
+
+        return decision.toString();
+    }
+
 
     public JSONObject getHeadingLeft() { return command.get(Action.HEADING_LEFT); }
 
@@ -59,3 +126,46 @@ public class CommandBook {
         this.command.put(action, decision); // putting decision into the HashMap, an ex. of action (the key) is Action.STOP
     }
 }
+
+/*
+
+public class Drone {
+
+    private Integer battery;
+    private Compass compass;
+
+    public Drone(Integer battery, Direction heading) {
+        this.battery = battery;
+        this.compass = new Compass(heading);
+    }
+
+    public Integer getBattery() {
+        return this.battery;
+    }
+
+    public Direction getHeading() {
+        return this.compass.getHeading();
+    }
+
+
+    public String turnLeft() {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
+
+        decision.put("action", "fly");
+        decision.put("parameters", parameters.put("direction", this.compass.getLeft()));
+
+        return decision.toString();
+    }
+
+    public String turnRight() {
+        JSONObject decision = new JSONObject();
+        JSONObject parameters = new JSONObject();
+
+        decision.put("action", "fly");
+        decision.put("parameters", parameters.put("direction", this.compass.getRight()));
+
+        return decision.toString();
+    }
+}
+*/
