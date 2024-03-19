92a40865c5245b78b27929967a8f887e3e78c2f7
Anindita Bornomala <aninborno@LAPTOP>
2024-03-19 02:44:58 -0400

    Resolved error in Translator class, added temporary ECHO enum

--- src/main/java/ca/mcmaster/se2aa4/island/team220/Action.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/Action.java
@@ -3,6 +3,7 @@ package ca.mcmaster.se2aa4.island.team220;
 public enum Action {
     //refactor gridSearch by hiding instances of decision.put in a map in class Action.java
     FLY,
+    ECHO, // MIGHT NOT NEED THIS, DELETE LATER
     ECHO_NORTH,
     ECHO_SOUTH,
     ECHO_EAST,
