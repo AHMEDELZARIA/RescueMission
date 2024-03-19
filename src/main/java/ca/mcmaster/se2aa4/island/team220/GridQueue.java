332aae6ba0d1745af5f05382d3d56d2651d6197c
neha-bhatla <nehab2708@gmail.com>
2024-03-19 18:05:44 -0400

    calls command class and creates queue

--- src/main/java/ca/mcmaster/se2aa4/island/team220/GridQueue.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/GridQueue.java
@@ -1,10 +1,17 @@
 package ca.mcmaster.se2aa4.island.team220;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class GridQueue {
 
     private Queue<String> queue;
+
+    private CommandBook commands;
+
+    public GridQueue() {
+        queue = new LinkedList<>();
+    }
 
     
     
