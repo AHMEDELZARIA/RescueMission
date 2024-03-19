90d52237858ca7821bd7dfdb55a32d2defde9567
Anindita Bornomala <aninborno@LAPTOP>
2024-03-19 03:18:16 -0400

    Updated Action enum class

--- src/main/java/ca/mcmaster/se2aa4/island/team220/Action.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/Action.java
@@ -3,13 +3,14 @@ package ca.mcmaster.se2aa4.island.team220;
 public enum Action {
     //refactor gridSearch by hiding instances of decision.put in a map in class Action.java
     FLY,
-    ECHO, // MIGHT NOT NEED THIS, DELETE LATER
-    ECHO_NORTH,
-    ECHO_SOUTH,
-    ECHO_EAST,
-    ECHO_WEST,
-    HEADING_LEFT,
-    HEADING_RIGHT,
+    ECHO,
+    ECHO_NORTH, // MIGHT NOT NEED THIS
+    ECHO_SOUTH, // MIGHT NOT NEED THIS
+    ECHO_EAST, // MIGHT NOT NEED THIS
+    ECHO_WEST, // MIGHT NOT NEED THIS
+    HEADING,
+    HEADING_LEFT, // MIGHT NOT NEED THIS
+    HEADING_RIGHT, // MIGHT NOT NEED THIS
     SCAN,
     STOP;
 }
