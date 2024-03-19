d5609a4488cce4b532c3046537adb33d68a889cf
Anindita Bornomala <aninborno@LAPTOP>
2024-03-19 13:12:06 -0400

    Updated CommandBook class

--- src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
@@ -38,9 +38,9 @@ public class CommandBook {
 
     public JSONObject getHeadingRight() { return command.get(Action.HEADING_RIGHT); }
 
-    public void updateHeadingLeft(Compass compass) { buildAction(Action.HEADING_LEFT, compass.turnLeft().toString()); }
+    public void updateHeadingLeft(Compass compass) { buildAction(Action.HEADING_LEFT, compass.turnLeft().toString()); } // MIGHT NOT NEED
     
-    public void updateHeadingRight(Compass compass) { buildAction(Action.HEADING_RIGHT, compass.turnRight().toString()); }
+    public void updateHeadingRight(Compass compass) { buildAction(Action.HEADING_RIGHT, compass.turnRight().toString()); } // MIGHT NOT NEED
 
     public void buildAction(Action action, String direction) { // Action are the enums
         JSONObject decision = new JSONObject();
@@ -52,6 +52,6 @@ public class CommandBook {
             decision.put("parameters", parameters);
         }
 
-        this.command.put(action, decision);
+        this.command.put(action, decision); // putting decision into the HashMap, an ex. of action (the key) is Action.STOP
     }
 }
