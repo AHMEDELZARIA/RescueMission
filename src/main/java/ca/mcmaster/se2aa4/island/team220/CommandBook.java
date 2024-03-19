6ec333fdc8c7c99d7ca9617d13a78b2686175248
Anindita Bornomala <aninborno@LAPTOP>
2024-03-19 02:33:14 -0400

    build action commands in CommandBook class

--- src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/CommandBook.java
@@ -5,16 +5,41 @@ import org.json.JSONObject;
 
 public class CommandBook {
 
-    private HashMap<String, JSONObject> command;
+    private Action action;
+    private Direction heading;
+    private Compass compass;
+
+    private HashMap<Action, JSONObject> command;
     JSONObject decision = new JSONObject();
+    JSONObject parameters = new JSONObject();
+     
 
     public CommandBook() {
-        command = new HashMap<>();
-        this.command.put("stop", decision.put("action", "stop"));
+
+        this.command = new HashMap<>();
+        decision.put("action", "stop");
+        // this.command.put("stop", decision);
     }
 
-    public JSONObject getCommand() {
-        return command.get("stop");
+    public void buildStop() {
+        this.decision.put("action", "stop");
+        this.command.put(Action.STOP, decision);
+        // return command.get(Action.STOP);
+    }
+    public void buildFly() {
+        this.decision.put("action", "fly");
+        this.command.put(Action.FLY, decision);
+    }
+
+    public void buildScan() {
+        this.decision.put("action", "scan");
+        this.command.put(Action.SCAN, decision);
+    }
+
+    public void buildEchoNorth() {
+        decision.put("action", "echo");
+        decision.put("parameters", parameters.put("direction", "N"));
+        this.command.put(Action.ECHO, decision);
     }
 
     
