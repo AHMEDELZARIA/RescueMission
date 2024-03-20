e8aa5bd16dd381e8d0aa36665500a5b99665b00b
Anindita-Bornomala <anindita.bornomala@gmail.com>
2024-03-20 03:54:41 -0400

    Updated refillQueue() by filling in refill methods

--- src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/GridSearch.java
@@ -10,16 +10,16 @@ public class GridSearch implements ISearchAlgorithm {
     private final Logger logger = LogManager.getLogger(); // for logger instructions
     private Compass compass; // creates a compass DELETE LATER
     private CommandBook command = new CommandBook(); // ADDED 19/03
-    private Information results; // added 19/03 DELETE LATER
+    // private Information results; // added 19/03 DELETE LATER
 
-    private int count = 0; // helps run different actions in a round, gets 'reset'
+    // private int count = 0; // helps run different actions in a round, gets 'reset'
     private int searchCount = 0; // keeps track of number of times searchSite() is run
-    private String found = ""; // returns "GROUND" or "OUT_OF_RANGE", aka 'echo' results
-    private int range = 0; // returns the range of the 'echo' result
-    private String scanBiomes = ""; // returns the first biome found from 'scan' results 
+    // private String found = ""; // returns "GROUND" or "OUT_OF_RANGE", aka 'echo' results
+    // private int range = 0; // returns the range of the 'echo' result
+    // private String scanBiomes = ""; // returns the first biome found from 'scan' results 
     private String scanSites = ""; // returns the site if the site is found from 'scan' results 
-    private boolean down = false; // determines whether the drone is facing upwards or downwards when it exits the island for intoPosition()
-    private boolean interlaceCheck = false;
+    private boolean down = true; // determines whether the drone is facing upwards or downwards when it exits the island for intoPosition()
+    // private boolean interlaceCheck = false;
 
     JSONObject decision = new JSONObject();
     JSONObject parameters = new JSONObject();
@@ -47,55 +47,64 @@ public class GridSearch implements ISearchAlgorithm {
         return null;
     }
 
-    public void refillQueue() {
+    public void refillQueue(String found, String biome, Compass compass, boolean interlaceCheck) {
         switch (this.currentMode) {
             case 0:
-                if (this.found == "GROUND") {
+                if (found == "GROUND") {
                     this.currentMode = 1;
                 } else {
-                    // execute mode0
+                    refillFindIsland(); // execute mode0
                 }
+                break;
             case 1:
-                //execute mode1
+                refillFaceIsland(null); // execute mode1
                 this.currentMode = 2;
+                break;
             case 2:
-                if (this.scanBiomes != "OCEAN") {
+                if (biome != "OCEAN") {
                     this.currentMode = 3;
                 } else {
-                    // execute mode2
+                    refillReachIsland(); // execute mode2
                 }
+                break;
             case 3:
-                if (this.scanBiomes == "OCEAN") {
+                if (biome == "OCEAN") {
                     this.currentMode = 4;
                 } else {
-                    // execute mode3
+                    refillSearchSite(); // execute mode3
                 }
+                break;
             case 4:
-                if (this.found == "OUT_OF_RANGE") {
+                if (found == "OUT_OF_RANGE") {
                     this.currentMode = 5;
                 } else {
-                    // execute mode4
+                    refillIntoPosition(); // execute mode4
                 }
+                break;
             case 5:
-                // execute mode5
+                refillInterlaceA(compass); // execute mode5
                 this.currentMode = 6;
+                break;
             case 6:
-                // execute mode6
+                refillInterlaceB(); // execute mode6
                 // return found
-                if (this.found == "GROUND") {
+                if (found == "GROUND") {
                     this.currentMode = 2;
                 } else {
-                    if (this.interlaceCheck == true) {
+                    if (interlaceCheck == true) {
                         this.currentMode = 8;
                     } else {
                         this.currentMode = 7;
                     }
                 }
+                break;
             case 7:
-                // execute mode7
+                refillInterlaceC(compass); // execute mode7
                 this.currentMode = 2;
+                break;
             case 8:
-                // execute stop
+                queue.enqueue(command.getStop()); // use stop command, end gridSearch
+                break;
         }
     }
 
@@ -146,7 +155,7 @@ public class GridSearch implements ISearchAlgorithm {
         queue.enqueue(command.getFly());
     }
 
-    public void refillInterlaceA() {
+    public void refillInterlaceA(Compass compass) {
         if (this.down == true) {
             queue.enqueue(command.getTurnLeft(compass));
             queue.enqueue(command.getTurnLeft(compass));
@@ -164,7 +173,7 @@ public class GridSearch implements ISearchAlgorithm {
         }
     }
 
-    public void refillInterlaceC() {
+    public void refillInterlaceC(Compass compass) {
         if (this.searchCount % 2 == 1) {
             if (this.down == true) {
                 queue.enqueue(command.getTurnRight(compass));
