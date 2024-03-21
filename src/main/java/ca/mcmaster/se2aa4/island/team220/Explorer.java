fc1f909ea05b95b87fa222ce592b0f98d5bc04a0
Anindita-Bornomala <anindita.bornomala@gmail.com>
2024-03-20 20:25:13 -0400

    SUCCESSFULLY RAN GRIDSEARCH ALGORITHM ON MAP03 :^D

--- src/main/java/ca/mcmaster/se2aa4/island/team220/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/Explorer.java
@@ -53,7 +53,7 @@ public class Explorer implements IExplorerRaid {
         // this.count++;
         logger.info(this.count);
 
-        if (count < 2000) { //853
+        if (count < 1650) { //853
             this.decision = search.makeDecision(results.getFound(), results.getBiome(), compass);
             this.count++;
         } else {
