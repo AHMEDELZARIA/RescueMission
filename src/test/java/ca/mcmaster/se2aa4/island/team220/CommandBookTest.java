c5f9ff85504f845e3e8a9b91c739623dfdd9fd1e
neha-bhatla <nehab2708@gmail.com>
2024-03-21 12:55:49 -0400

    Created test for getStop method from CommandBook class

--- src/test/java/ca/mcmaster/se2aa4/island/team220/CommandBookTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team220/CommandBookTest.java
@@ -1,5 +1,19 @@
 package ca.mcmaster.se2aa4.island.team220;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CommandBookTest {
     
+    private Compass compass;
+
+    public void testGetStop(){
+        CommandBook testCommand = new CommandBook(); 
+
+        //lets test the string we should be returned when we stop the actions
+        String correct = "{\"action\":\"stop\"}";
+        String output = testCommand.getStop();
+        assertEquals(correct, output);
+    }
 }
