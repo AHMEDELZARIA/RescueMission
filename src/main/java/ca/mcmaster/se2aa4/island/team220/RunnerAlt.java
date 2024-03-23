f8da22d3b296d4f061e23ab4133db912ec188c72
Anindita-Bornomala <anindita.bornomala@gmail.com>
2024-03-23 16:36:46 -0400

    Updated and renamed Runner class to RunnerAlt for non-interference in main

--- src/main/java/ca/mcmaster/se2aa4/island/team220/RunnerAlt.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/RunnerAlt.java
@@ -4,22 +4,22 @@ import static eu.ace_design.island.runner.Runner.run;
 
 import java.io.File;
 
-public class Runner {
+public class Runner { // Runner is the Main class
 
     public static void main(String[] args) {
-        String filename = args[0];
+        String filename = args[0]; // where we get the map argument
         try {
-            run(Explorer.class)
+            run(Explorer.class) // then we run the whole Explorer class (aka Command Centre gets the map for the mission)
                     .exploring(new File(filename))
                     .withSeed(42L)
-                    .startingAt(150, 70, "WEST")
-                    .backBefore(50000)
+                    .startingAt(1, 1, "EAST") // Original: 1,1 , EAST // Limit: 157 (52x3 + 1) to 159 (technically goes past but works)
+                    .backBefore(30000) // Original: 7000 // Max budget if start at top-left: 24000
                     .withCrew(5)
                     .collecting(1000, "WOOD")
                     .storingInto("./outputs")
                     .withName("Island")
                     .fire();
-        } catch (Exception e) {
+        } catch(Exception e) {
             System.err.println(e.getMessage());
             e.printStackTrace(System.err);
             System.exit(1);
