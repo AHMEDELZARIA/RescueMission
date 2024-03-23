4c343dc94737022f76c26bf35a755683800baa99
Anindita-Bornomala <anindita.bornomala@gmail.com>
2024-03-23 15:04:50 -0400

    Updated GridQueue class

--- src/main/java\ca\mcmaster\se2aa4\island\team220/GridQueue.java
+++ src/main/java\ca\mcmaster\se2aa4\island\team220/GridQueue.java
@@ -5,33 +5,25 @@ import java.util.Queue;
 
 public class GridQueue {
 
-    private Queue<String> queue;
-
-    // private CommandBook commands;
+    public Queue<String> queue;
 
     public GridQueue() {
         queue = new LinkedList<>();
     }
+    public void enqueue(String action) { queue.add(action); }
 
-    public void enqueue(String action){
-        queue.add(action); 
-    }
+    public String dequeue() { return queue.poll(); }
 
-    public String dequeue(){
-        if (queue.isEmpty()){
-            refillQueue();
-        }
-        return queue.poll();
-    }
-
-    public void refillQueue(){
-    }
-    
     public Boolean isEmpty() {
         if (queue.isEmpty() == true) {
             return true;
         } else {
             return false;
         }
+    }
+
+    // ADDED THIS RETURNS THE QUEUE (for testing)
+    public Queue<String> getQueue() {
+        return this.queue;
     }
 }
