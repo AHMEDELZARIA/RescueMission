15940d1a4d5a97920c52b418a9c39e80ac5fbd08
Anindita-Bornomala <anindita.bornomala@gmail.com>
2024-03-24 19:02:48 -0400

    Updated Compass class to match main

--- src/main/java/ca/mcmaster/se2aa4/island/team220/Compass.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team220/Compass.java
@@ -4,18 +4,27 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class Compass {
+    private Direction initialHeading;
     private Direction heading;
+    private Direction prevHeading;
     private Map<Direction, Direction> goingRight;
     private Map<Direction, Direction> goingLeft;
 
     public Compass(Direction heading) {
         this.heading = heading;
+        this.prevHeading = heading;
+        this.initialHeading = heading;
         buildCompass();
     }
 
     public Direction getHeading() {
         return this.heading;
     }
+
+    public Direction getPrevHeading() { return this.prevHeading; }
+
+    public void updatePrevHeading() { this.prevHeading = this.heading; }
+    public Direction getInitialHeading() { return this.initialHeading; }
 
     public void buildCompass() {
         // Define turn right logic
@@ -34,11 +43,13 @@ public class Compass {
     }
 
     public Direction turnLeft() {
+        this.prevHeading = this.heading;
         this.heading = goingLeft.get(this.heading);
         return this.heading;
     }
 
     public Direction turnRight() {
+        this.prevHeading = this.heading;
         this.heading = goingRight.get(this.heading);
         return this.heading;
     }
@@ -50,4 +61,5 @@ public class Compass {
     public Direction getRight() {
         return goingRight.get(this.heading);
     }
+
 }
