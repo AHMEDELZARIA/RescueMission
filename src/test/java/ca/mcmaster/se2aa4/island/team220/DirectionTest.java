217c8c9d6abed5a108deedc0f4cfb95fb7629407
Ahmed Elzaria <aelzaria@gmail.com>
2024-03-24 23:31:19 -0400

    FINAL...maybe..

--- src/test/java/ca/mcmaster/se2aa4/island/team220/DirectionTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team220/DirectionTest.java
@@ -1,6 +1,5 @@
 package ca.mcmaster.se2aa4.island.team220;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class DirectionTest {
